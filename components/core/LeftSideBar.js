3d187fd40e046b617d697d51a350faad1db0b6f5
saviomartin <saviomartin2007@gmail.com>
2021-08-05 12:07:49 +0530

    feat: created a ripple btn

--- components/core/LeftSideBar.js
+++ components/core/LeftSideBar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { FiChevronLeft } from "react-icons/fi";
+import { Btn } from "..";
 
 const LeftSideBar = () => {
   return (
     <div className="w-[36%] h-full bg-[#fff] shadow relative border-r border-[#ddd]">
       Left
       <div className="absolute top-0 right-[-21px] h-full flex items-center justify-center">
-        <button className="bg-[#ddd] w-[21px] h-[95px] tab relative p-[1px]">
-          <div className="bg-[#fff] tab h-full w-full flex items-center justify-center">
-            <FiChevronLeft className="text-[#222]" />
-          </div>
-        </button>
+        <Btn className="tab">
+          <button className="bg-[#ddd] w-[21px] h-[95px] tab relative p-[1px]">
+            <div className="bg-[#fff] tab h-full w-full flex items-center justify-center">
+              <FiChevronLeft className="text-[#222]" />
+            </div>
+          </button>
+        </Btn>
       </div>
     </div>
   );
--- components/index.js
+++ components/index.js
@@ -1,3 +1,6 @@
 // core components
 export { default as LeftSideBar } from "./core/LeftSideBar";
 export { default as PlayGround } from "./core/PlayGround";
+
+// util components
+export { default as Btn } from "./utils/Btn";
