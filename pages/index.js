34050b165e95aed5b12f8588dcb6e0acbb40f193
saviomartin <saviomartin2007@gmail.com>
2021-08-22 21:23:35 +0530

    feat: initialised supabase

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,149 @@
-import Head from 'next/head'
+import { useUser } from "@auth0/nextjs-auth0";
+import { Btn, FancyBtn } from "../components";
+import Link from "next/link";
+
+import {
+  BsArrowBarLeft,
+  BsFillPersonPlusFill,
+  BsHouse,
+  BsWindow,
+} from "react-icons/bs";
+
+import { Menu, MenuList, MenuButton, MenuItem } from "@reach/menu-button";
+import { FiArrowRight, FiGithub, FiLogOut } from "react-icons/fi";
+import { useEffect, useState } from "react";
+import { supabase } from "../components/supabaseClient";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  const [templates, setTemplates] = useState({
+    home: [],
+    article: [],
+  });
+
+  const fetchHomeTemplates = async () => {
+    let { data, error } = await supabase.from("home").select("*");
+
+    return data;
+  };
+
+  const fetchArticleTemplates = async () => {
+    let { data, error } = await supabase.from("article").select("*");
+
+    return data;
+  };
+
+  useEffect(async () => {
+    setTemplates({
+      home: await fetchHomeTemplates(),
+      article: await fetchArticleTemplates(),
+    });
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+    <div className="w-full h-full flex items-center justify-center flex-col bg-[#f7f9fa]">
+      <div className="gradient pb-[2px] w-full">
+        <header className="w-full flex items-center justify-between p-3 pr-[2rem] bg-[#fff]">
+          <div className="flex items-center">
+            <h1 className="flex items-center justify-center">
+              <span className="text-3xl font-extrabold ml-2 gradient-text bg-app-graient-to-l">
+                Slickr
+              </span>
+            </h1>
+          </div>
+          <div className="flex items-center">
+            {user ? (
+              <Menu>
+                <MenuButton>
+                  <Btn className="!rounded-md !ml-[10px]">
+                    <div className="glassmorphism p-[2px] rounded-full">
+                      <img
+                        src={user.picture}
+                        alt="USER"
+                        className="h-[40px] rounded-full"
+                      />
+                    </div>
+                  </Btn>
+                </MenuButton>
+                <MenuList className="bg-white mt-1 p-1 rounded-md shadow z-[20] relative border border-[#ddd]">
+                  <MenuItem>
+                    <Link href="/api/auth/logout">
+                      <a>
+                        <Btn className="!rounded-md">
+                          <div className="glassmorphism py-2 px-3 rounded-md flex items-center justify-center shadow">
+                            <FiLogOut className="text-xl mr-2" />
+                            Logout
+                          </div>
+                        </Btn>
+                      </a>
+                    </Link>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            ) : (
+              <Link href="/api/auth/login">
+                <a>
+                  <Btn className="!rounded-md !ml-[5px]">
+                    <div className="border-2 border-[#a770ef] text-[#a770ef] py-[6px] px-3 rounded-md flex items-center justify-center shadow capitalize">
+                      <BsFillPersonPlusFill className="text-xl mr-2" />
+                      Sign In
+                    </div>
+                  </Btn>
+                </a>
+              </Link>
+            )}
+          </div>
+        </header>
+      </div>
+      <div className="gradient-left w-10/12 rounded-md p-6 mt-4 shadow-lg">
+        <h1 className="font-bold text-[2.7em] text-[#fff] w-[70%] leading-[1.25em]">
+          The Next-Generation CSS Builder
+          <div className="flex items-center">
+            in the web for your
+            <a
+              href="https://hashnode.com/"
+              className="text-[#3068FA] bg-app-graient-to-l relative ml-3"
+            >
+              hashnode blog
+              <div className="absolute bottom-1 left-0 bg-app-graient-to-l h-[3px] w-full"></div>
+            </a>
+            <img
+              src="/assets/hashnode-logo.png"
+              alt="Logo"
+              className="w-[40px] h-[40px] ml-2"
+            />
+          </div>
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+        <p className="text-[#eee] mt-4 text-xs lg:text-base xl:text-base w-[60%]">
+          Super Charge your hashnode blog with styler. Write your style in the
+          browers, grab the code and start using ⚡️ Wohoo, its that easy!
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="mt-5 flex">
+          <FancyBtn
+            text="Start Creating"
+            icon={<FiArrowRight className="text-xl ml-2" />}
+          />
+          <div className="ml-2">
+            <FancyBtn
+              text="Star on Github"
+              icon={<FiGithub className="text-xl ml-2" />}
+            />
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
+      <div className="bg-[#fff] shadow-lg w-10/12 border border-[#ddd] rounded-md my-4 p-6">
+        <h1 className="font-bold text-3xl text-[#333]">Templates</h1>
+        <p className="text-sm text-[#777] mt-2">
+          Start from a scratch or choose a template
+        </p>
+        <div className="w-full bg-[#f7f9fa] border border-[#ddd] rounded-md my-2 mt-4 p-6">
+          <h1 className="font-bold text-xl text-[#333] flex items-center">
+            Home <FiArrowRight className="ml-1 text-lg" />
+          </h1>
+          {JSON.stringify(templates)}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
