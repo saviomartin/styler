698ad1f62ac85d283235bef78c1f17439e663863
saviomartin <saviomartin2007@gmail.com>
2021-08-26 19:12:56 +0530

    feat: add header

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,9 @@ import { generateCSS } from "../components/helper";
 // auth0 wrapper
 import { UserProvider } from "@auth0/nextjs-auth0";
 
+// Head
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   const [data, setData] = useState(mockData);
   const [basicStyles, setBasicStyles] = useState(
@@ -38,6 +41,33 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <UserProvider>
+      <Head>
+        <title>
+          Styler - The Next-Generation CSS Builder in the web for your hashnode
+          blog
+        </title>
+        <link rel="icon" href="/assets/icon.png" />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Styler - The Next-Generation CSS Builder in the web for your hashnode blog"
+        />
+        <meta
+          property="og:image"
+          content="https://styler.vercel.app/assets/banner.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:title"
+          content="Styler - The Next-Generation CSS Builder in the web for your hashnode blog"
+        />
+        <meta
+          property="twitter:image"
+          content="https://styler.vercel.app/assets/banner.png"
+        />
+      </Head>
       <Component {...pageProps} {...props} />
     </UserProvider>
   );
